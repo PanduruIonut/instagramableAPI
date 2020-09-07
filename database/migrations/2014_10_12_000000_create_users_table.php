f5309786cad8033e0d33659c1beec96318f58396
Panduru Ionut <panduru.ionut@hotmail.com>
2020-09-07 13:03:32 +0300

    add user table fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->increments('id');
+            $table->string('username')->unique();
+            $table->string('profile_picture');
+            $table->string('full_name');
+            $table->string('bio')->nullable();
+            $table->string('website')->nullable();
             $table->timestamps();
         });
     }
